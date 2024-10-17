76bea559c19edbe8fb5273773a871b6f373f88b6
SangLv <lsang2884@gmail.com>
2024-10-17 18:33:17 +0700

    new bluetooth

--- Pages/HomePage/HomePage.xaml.cs
+++ Pages/HomePage/HomePage.xaml.cs
@@ -1,24 +1,19 @@
-using Bluetooth.Services;
-using System.Windows.Input;
-
 namespace Bluetooth.Pages
 {
     public partial class HomePage : ContentPage
     {
-        public BluetoothManager? bluetoothManager;
-
         public HomePage()
         {
             InitializeComponent();
             try
             {
-                bluetoothManager = new BluetoothManager();
+                BluetoothServiceManager bluetoothServiceManager = new BluetoothServiceManager();
+                bluetoothServiceManager.StartBluetooth();
             }
             catch (Exception e)
             {
                 System.Diagnostics.Debug.WriteLine($"Message => {e.Message}");
             }
-            BindingContext = bluetoothManager;
         }
 
         private void DevicesListView_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -27,7 +22,6 @@ namespace Bluetooth.Pages
             BluetoothDevice? selectedDevice = e.Item as BluetoothDevice;
             if (selectedDevice != null)
             {
-                _ = bluetoothManager!.ConnectToDeviceAsync(selectedDevice!);
             }
         }
     }
--- Services/BluetoothManager.cs
+++ Services/BluetoothManager.cs
@@ -1,154 +1,39 @@
-﻿using System.Collections.ObjectModel;
-using Plugin.BLE;
-using Plugin.BLE.Abstractions.Contracts;
+﻿using CoreBluetooth;
 
-namespace Bluetooth.Services
+public class BluetoothServiceManager
 {
-    public class BluetoothManager
+    public void StartBluetooth()
     {
-        private readonly IBluetoothLE _bluetoothLe;
-        private readonly IAdapter _adapter;
-        private BluetoothDevice? _deviceConnected;
-        public ObservableCollection<BluetoothDevice> Devices { get; set; }
+        var centralManager = new CBCentralManager(new CentralManagerDelegate(), null);
+        System.Diagnostics.Debug.WriteLine($"Bluetooth started on iOS {centralManager.State}");
+    }
+}
 
-        public BluetoothManager()
+
+public class CentralManagerDelegate : CBCentralManagerDelegate
+{
+    public override void UpdatedState(CBCentralManager central)
+    {
+        switch (central.State)
         {
-            _bluetoothLe = CrossBluetoothLE.Current;
-            _adapter = CrossBluetoothLE.Current.Adapter;
-            Devices = new ObservableCollection<BluetoothDevice>();
-
-            _adapter.DeviceDiscovered += (s, a) =>
-            {
-                if (!Devices.Any(d => d.Id.ToString() == a.Device.Id.ToString()) && !string.IsNullOrEmpty(a.Device.Name))
-                {
-                    Devices.Add(new BluetoothDevice { Name = a.Device.Name, Id = a.Device.Id.ToString(), Device = a.Device, Rssi = a.Device.Rssi, IsConnecting = false, IsConnected = false });
-                }
-            };
-
-            _adapter.DeviceConnected += async (s, a) =>
-            {
-                System.Diagnostics.Debug.WriteLine($"Connect ==> {a.Device.Name} - {a.Device.Rssi}");
-                foreach (var dev in Devices)
-                {
-                    if (dev.Id.ToString() == a.Device.Id.ToString())
-                    {
-                        dev.IsConnected = true;
-                        _deviceConnected = dev;
-                        await GetDeviceServicesAndCharacteristicsAsync(dev);
-                    }
-                }
-            };
-
-            _adapter.DeviceDisconnected += (s, a) =>
-            {
-                System.Diagnostics.Debug.WriteLine($"Disconnected ==> {a.Device.Name}");
-                foreach (var dev in Devices)
-                {
-                    if (dev.Id.ToString() == a.Device.Id.ToString())
-                    {
-                        dev.IsConnected = false;
-                    }
-                }
-            };
-
-            _bluetoothLe.StateChanged += (sender, args) =>
-            {
-                if (_bluetoothLe.IsOn)
-                {
-                    MainThread.BeginInvokeOnMainThread(async () =>
-                    {
-                        await ScanForDevicesAsync();
-                        SortDevicesByRssi();
-                    });
-                }
-            };
+            case CBManagerState.PoweredOn:
+                System.Diagnostics.Debug.WriteLine("Bluetooth is powered on.");
+                break;
+            case CBManagerState.PoweredOff:
+                System.Diagnostics.Debug.WriteLine("Bluetooth is powered off.");
+                break;
+            case CBManagerState.Unauthorized:
+                System.Diagnostics.Debug.WriteLine("Bluetooth is not authorized.");
+                break;
+            case CBManagerState.Unsupported:
+                System.Diagnostics.Debug.WriteLine("Bluetooth is not supported on this device.");
+                break;
+            case CBManagerState.Unknown:
+                System.Diagnostics.Debug.WriteLine("Bluetooth is Unknown");
+                break;
+            default:
+                System.Diagnostics.Debug.WriteLine("Bluetooth state changed.");
+                break;
         }
-
-        public async Task ScanForDevicesAsync()
-        {
-            if (!_bluetoothLe.IsOn) return;
-            Devices.Clear();
-            await _adapter.StartScanningForDevicesAsync();
-        }
-
-        public async Task ConnectToDeviceAsync(BluetoothDevice device)
-        {
-            if (device.IsConnected || device.IsConnecting) return;
-            if (_deviceConnected != null)
-            {
-                await DisConnectToDeviceAsync(_deviceConnected);
-            }
-            device.IsConnecting = true;
-            await _adapter.ConnectToDeviceAsync(device.Device);
-            device.IsConnecting = false;
-        }
-
-        public async Task DisConnectToDeviceAsync(BluetoothDevice device)
-        {
-            _deviceConnected = null;
-            device.IsConnected = false;
-            await _adapter.DisconnectDeviceAsync(device.Device);
-        }
-
-        public void SortDevicesByRssi()
-        {
-            var sortedDevices = Devices.OrderByDescending(d => d.Rssi).ToList();
-            Devices.Clear();
-            foreach (var device in sortedDevices)
-            {
-                Devices.Add(device);
-            }
-        }
-
-        private async Task GetDeviceServicesAndCharacteristicsAsync(BluetoothDevice device)
-        {
-            try
-            {
-                if (device == null || device.Device == null)
-                {
-                    System.Diagnostics.Debug.WriteLine("Device or Device reference is null.");
-                    return;
-                }
-                var services = await device.Device.GetServicesAsync();
-
-                if (services == null || !services.Any())
-                {
-                    System.Diagnostics.Debug.WriteLine("No services found.");
-                    return;
-                }
-
-                System.Diagnostics.Debug.WriteLine("Services found:");
-                foreach (var service in services)
-                {
-                    System.Diagnostics.Debug.WriteLine($"Service: {service.Name} -- {service.Id}");
-                    var characteristics = await service.GetCharacteristicsAsync();
-                    if (characteristics == null || !characteristics.Any())
-                    {
-                        System.Diagnostics.Debug.WriteLine("No characteristics found for this service.");
-                        continue;
-                    }
-
-                    foreach (var characteristic in characteristics)
-                    {
-                        System.Diagnostics.Debug.WriteLine($"Characteristic: {characteristic.Uuid}");
-                        var result = await characteristic.ReadAsync();
-                        if (result.data.Length > 0)
-                        {
-                            var value = BitConverter.ToString(result.data).Replace("-", " ");
-                            System.Diagnostics.Debug.WriteLine($"Value: {value}");
-                        }
-                        else
-                        {
-                            System.Diagnostics.Debug.WriteLine("No value returned for this characteristic.");
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Error getting services and characteristics: {ex.Message}");
-            }
-        }
-
     }
 }
